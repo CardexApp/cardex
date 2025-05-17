f35ed6b941928383d69a04599c80f954a14ec487
Roseford <obianujuofodu@gmail.com>
2025-05-17 19:11:25 +0100

    added a comma

--- backend/CARDEX_BACKEND/CARDEX_BACKEND/settings.py
+++ backend/CARDEX_BACKEND/CARDEX_BACKEND/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework'
+    'rest_framework',
     'store',
     'django.contrib.admin',
     'django.contrib.auth',
