450b3ccab60f63121bf46a92483a2b3ed96a3ddb
Chukwubuokem Ogochukwu Mordi <250038040@aston.ac.uk>
2025-05-17 03:44:46 +0100

    Updated product page and header

---
+++ frontend/src/assets/asset.jsx
@@ -0,0 +1,12 @@
+import LOGO from "../../src/assets/LOGO.svg";
+import BENZ_1 from "../../src/assets/BENZ_1.avif";
+import BENZ_2 from "../../src/assets/BENZ_2.avif";
+import BENZ_3 from "../../src/assets/BENZ_3.avif";
+import BENZ_4 from "../../src/assets/BENZ_4.avif";
+
+
+const asset = {
+  LOGO, BENZ_1, BENZ_2, BENZ_3, BENZ_4
+};
+
+export default asset
