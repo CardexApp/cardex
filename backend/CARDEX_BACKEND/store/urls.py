4c5c3fc15509935d6e08ea5118cb0525d8e903f3
Roseford <obianujuofodu@gmail.com>
2025-05-17 22:38:25 +0100

    added urls

--- backend/CARDEX_BACKEND/store/urls.py
+++ backend/CARDEX_BACKEND/store/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from . import views
+from .views import (
+    ProductListView,
+    ProductDetailView,
+    CarTypeListView,
+    ProductSearchView,
+    GuestCheckoutView
+)
 
 urlpatterns = [
-    
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:id>/', ProductDetailView.as_view(), name='product-detail'),
+    path('car-types/', CarTypeListView.as_view(), name='car-types'),
+    path('products/search/', ProductSearchView.as_view(), name='product-search'),
+    path('guest-checkout/', GuestCheckoutView.as_view(), name='guest-checkout'),
 ]
