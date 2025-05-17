704cdfe6a0038e6a26acf2404b72f44424b043b3
Roseford <obianujuofodu@gmail.com>
2025-05-17 22:38:55 +0100

    added views

--- backend/CARDEX_BACKEND/store/views.py
+++ backend/CARDEX_BACKEND/store/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics, filters
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Product, CarType, Order
+from .serializers import (
+    ProductSerializer,
+    CarTypeSerializer,
+    OrderSerializer
+)
+
+# /api/products/ GET
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# /api/products/<id>/ GET
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
+
+# /api/car-types/ GET
+class CarTypeListView(generics.ListAPIView):
+    queryset = CarType.objects.all()
+    serializer_class = CarTypeSerializer
+
+# /api/products/search/ GET
+class ProductSearchView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description', 'mileage', 'model_year']
+    ordering_fields = ['price', 'model_year']
+
+# /api/guest-checkout/ POST
+class GuestCheckoutView(generics.CreateAPIView):
+    serializer_class = OrderSerializer
