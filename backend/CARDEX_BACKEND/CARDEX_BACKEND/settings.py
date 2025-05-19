0db5d855546ef4654f7b73b8d5c69bfd371c7a7b
Roseford <obianujuofodu@gmail.com>
2025-05-19 04:41:04 +0100

    Allowed CORS

--- backend/CARDEX_BACKEND/CARDEX_BACKEND/settings.py
+++ backend/CARDEX_BACKEND/CARDEX_BACKEND/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'store',
     'django.contrib.admin',
@@ -45,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'CARDEX_BACKEND.urls'
 
