ca88fe69f2412e67d35f0b6cc484e9bb59eea974
Roseford <obianujuofodu@gmail.com>
2025-05-19 04:50:45 +0100

    deleted the Allowed CORS changes

--- backend/CARDEX_BACKEND/CARDEX_BACKEND/settings.py
+++ backend/CARDEX_BACKEND/CARDEX_BACKEND/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'rest_framework',
     'store',
     'django.contrib.admin',
@@ -46,8 +45,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware', 
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,8 +53,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'CARDEX_BACKEND.urls'
 
