38cfbd08a46e7728707b7087336fa6e4bf826ad9
Faisal Sherif <116718026+faisalsherif7@users.noreply.github.com>
2025-06-02 16:45:25 +0100

    remove previous SQLite config from settings.py

--- backend/CARDEX_BACKEND/CARDEX_BACKEND/settings.py
+++ backend/CARDEX_BACKEND/CARDEX_BACKEND/settings.py
@@ -82,12 +82,6 @@ WSGI_APPLICATION = 'CARDEX_BACKEND.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 DATABASES = {
     'default': {
