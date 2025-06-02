a25dbb2c3522bbb1cabd747bf5513b8b6a69ef57
prince osei <240273600@aston.ac.uk>
2025-06-02 23:44:33 +0100

    Login functionality

--- frontend/src/Components/User/Login.jsx
+++ frontend/src/Components/User/Login.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Login.css"
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const loginSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        {username, password},
+      )
+    }
+  }
+
+
   return (
-    <div>
-      PRINCE OSEI
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ea officia consequuntur sapiente quidem ab nam eligendi. Officiis necessitatibus, iure fugit at praesentium velit vero distinctio quisquam, consectetur, quam ratione sunt laboriosam porro nam sapiente adipisci numquam natus tempora ut illo! Autem et, inventore eligendi repellendus amet explicabo pariatur earum quam illum nihil nostrum corporis rerum hic sed praesentium officiis? Molestiae aliquid temporibus aut iusto commodi debitis eum ducimus quis?</p>
+    <div className='loginContainer'>
+        <h2 className='loginTitle'>Welcome To Cardex</h2>
+        <p>Please Login to continue or SignUp to create an account</p>
+        <div className='loginInput'>
+            <img src="" alt="user avatar" className='loginAvatar' />
+            <form onSubmit={loginSubmit} action="">
+              <input type="text" placeholder=' Enter Username' className='loginUser' />
+              <input type="password" placeholder='Enter password' className='loginPass' />
+              <div className='loginRecover'><input type="checkbox" /> Remember me
+              <span className='loginForgotPass'>Forgot Password</span>
+              </div>
+              <button className='loginSubmit'>Submit</button>
+            </form>
+        </div>
     </div>
   )
+  
 }
 
 export default Login;
