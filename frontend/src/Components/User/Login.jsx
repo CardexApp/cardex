33e672867c4f4d8abdf6e1380587f5a41fb3a8ca
prince osei <240273600@aston.ac.uk>
2025-06-02 22:53:43 +0100

    Created login and login page

---
+++ frontend/src/Components/User/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      PRINCE OSEI
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ea officia consequuntur sapiente quidem ab nam eligendi. Officiis necessitatibus, iure fugit at praesentium velit vero distinctio quisquam, consectetur, quam ratione sunt laboriosam porro nam sapiente adipisci numquam natus tempora ut illo! Autem et, inventore eligendi repellendus amet explicabo pariatur earum quam illum nihil nostrum corporis rerum hic sed praesentium officiis? Molestiae aliquid temporibus aut iusto commodi debitis eum ducimus quis?</p>
+    </div>
+  )
+}
+
+export default Login;
