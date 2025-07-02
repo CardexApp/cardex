c213b9aeccd10a5d036913aa9f86da61f619b616
chukwuma johnpaul <chuzzychukwuma@gmail.com>
2025-07-02 18:25:36 +0100

    Added login API test using JWT token

--- backend/cardex_backend/store/tests.py
+++ backend/cardex_backend/store/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserLoginTest(APITestCase):
+    def setUp(self):
+        self.username = "testuser"
+        self.password = "testpass123"
+        User.objects.create_user(
+            username=self.username, password=self.password)
+
+    def test_user_can_login_with_valid_credentials(self):
+        url = reverse('token_obtain_pair')
+        data = {
+            "username": self.username,
+            "password": self.password,
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+
+class JWTExtraAuthTest(APITestCase):
+    def setUp(self):
+        self.username = "extratest"
+        self.password = "extratest123"
+        self.user = User.objects.create_user(
+            username=self.username, password=self.password)
+
+    def test_login_fails_with_wrong_password(self):
+        url = reverse('token_obtain_pair')
+        data = {
+            "username": self.username,
+            "password": "wrongpassword",
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotIn('access', response.data)
+
+    def test_refresh_token(self):
+        url = reverse('token_obtain_pair')
+        response = self.client.post(url, {
+            "username": self.username,
+            "password": self.password
+        })
+        refresh_token = response.data.get('refresh')
+        self.assertIsNotNone(refresh_token)
+
+        refresh_url = reverse('token_refresh')
+        refresh_response = self.client.post(
+            refresh_url, {"refresh": refresh_token})
+        self.assertEqual(refresh_response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', refresh_response.data)
